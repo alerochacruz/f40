7af5b25d022b3536573fa006d44719be6781f00e
Alejandro Rocha Cruz <alerochacruz@aol.com>
2024-10-05 08:48:58 -0300

    Fix Gnome Terminal installer script issues

--- gnome-terminal.sh
+++ gnome-terminal.sh
@@ -34,7 +34,9 @@ curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v0.3.0/insta
 # 2. Customize settings with dconf
 # ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # Retrieve the profile ID for visible-name='Catppuccin Mocha'
-PROFILE_ID=$(dconf dump /org/gnome/terminal/legacy/profiles:/ | grep -B 1 "background-color='#1e1e2e'" | head -n 1 | sed 's/\[://;s/\]//')
+# Mocha UUID: https://github.com/catppuccin/gnome-terminal/blob/main/install.py
+# PROFILE_ID=$(dconf dump /org/gnome/terminal/legacy/profiles:/ | grep -B 1 "background-color='#1e1e2e'" | head -n 1 | sed 's/\[://;s/\]//')
+PROFILE_ID=95894cfd-82f7-430d-af6e-84d168bc34f5
 
 if [ -z "$PROFILE_ID" ]; then
     echo "Profile ID not found."
@@ -43,13 +45,15 @@ fi
 
 # Apply settings to the profile
 dconf write /org/gnome/terminal/legacy/profiles:/:"$PROFILE_ID"/audible-bell false
+dconf write /org/gnome/terminal/legacy/profiles:/:"$PROFILE_ID"/use-system-font false
 dconf write /org/gnome/terminal/legacy/profiles:/:"$PROFILE_ID"/cell-height-scale 1.05
 dconf write /org/gnome/terminal/legacy/profiles:/:"$PROFILE_ID"/scrollbar-policy "'never'"
 dconf write /org/gnome/terminal/legacy/profiles:/:"$PROFILE_ID"/font "'Source Code Pro 15'"
 
 # Set the profile as default
 # Note: Replace 'your-profile-id-here' with the actual ID if you want to hardcode it or keep it dynamic as below
-dconf write /org/gnome/terminal/legacy/default-profile "'$PROFILE_ID'"
+# dconf write /org/gnome/terminal/legacy/default-profile "'$PROFILE_ID'"
+gsettings set org.gnome.Terminal.ProfilesList default "$PROFILE_ID"
 
 echo "Profile settings applied and set as default for ID: $PROFILE_ID"
 
