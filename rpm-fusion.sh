82318fb106bf5b0cae0aa899d41fdcbc5931ea42
Alejandro Rocha Cruz <alerochacruz@aol.com>
2024-12-29 16:23:27 -0300

    Fix RPM Fusion non-free mirror URL

--- rpm-fusion.sh
+++ rpm-fusion.sh
@@ -22,7 +22,7 @@ sudo dnf install --assumeyes \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf install --assumeyes \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 
 # 2. Switch to full ffmpeg
