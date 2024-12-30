0d94166bf3fb8ae2ee6c4c3dc4b1ac265130fc94
Alejandro Rocha Cruz <alerochacruz@aol.com>
2024-12-30 20:41:46 -0300

    Add script for font aliases

---
+++ font-aliases.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+set -x
+shopt -s failglob
+
+
+# 1. Create fontconfig directory
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+mkdir --parents ~/.config/fontconfig/
+
+
+# 2. Add font aliases
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+cat << "EOF" >> ~/.config/fontconfig/fonts.conf
+<?xml version='1.0'?>
+<!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+<fontconfig>
+  <alias>
+    <family>sans-serif</family>
+    <prefer><family>Inter</family></prefer>
+  </alias>
+  <match>
+    <test name="family"><string>Arial</string></test>
+    <edit name="family" mode="assign" binding="strong">
+      <string>Inter</string>
+    </edit>
+  </match>
+  <match>
+    <test name="family"><string>Noto Sans</string></test>
+    <edit name="family" mode="assign" binding="strong">
+      <string>Inter</string>
+    </edit>
+  </match>
+  <match>
+    <test name="family"><string>Liberation Sans</string></test>
+    <edit name="family" mode="assign" binding="strong">
+      <string>Inter</string>
+    </edit>
+  </match>
+  <match>
+    <test name="family"><string>Nimbus Sans L</string></test>
+    <edit name="family" mode="assign" binding="strong">
+      <string>Inter</string>
+    </edit>
+  </match>
+  <match>
+    <test name="family"><string>Open Sans L</string></test>
+    <edit name="family" mode="assign" binding="strong">
+      <string>Inter</string>
+    </edit>
+  </match>
+</fontconfig>
+EOF
+
